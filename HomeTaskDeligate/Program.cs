784cf1184ec09c3b22e8dd9a5d214cdf4330ec83
mammadlinurlan <nurlanym@code.edu.az>
2021-10-12 19:24:25 +0400

    Add project files.

---
+++ HomeTaskDeligate/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HomeTaskDeligate
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Generic<int> gnr1 = new Generic<int>(YoxlamaMetodu);
+            gnr1.Invoke(101);
+
+        }
+        public delegate void Generic<T>(T n);
+
+        public static void YoxlamaMetodu(int num)
+        {
+            if (num > 0)
+            { Console.WriteLine("eded musbetdir"); }
+            else
+            { Console.WriteLine("eded menfidir"); }
+            if (num % 2 == 0)
+            { Console.WriteLine("eded cutdur"); }
+            else
+            { Console.WriteLine("eded tekdir"); }
+            if (num % 2 == 0 || num % 3 == 0)
+            { Console.WriteLine("eded murekkebdir"); }
+            else
+            { Console.WriteLine("eded sadedir"); }
+        }
+    }
+}
